f7c29199a67d1b0e0fec2d407eb924148793218c
bkitchener1 <kitchener.brian@gmail.com>
2014-05-12 12:00:37 -0600

    adding project structure

---
+++ src/main/java/com/prototest/app/App.java
@@ -0,0 +1,13 @@
+package com.prototest.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
