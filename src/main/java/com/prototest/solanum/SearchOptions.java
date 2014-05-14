5f8d26bce65527ea5e738494fa0ff84878fbc237
Brian Kitchener <kitchener.brian@gmail.com>
2014-05-14 12:37:20 -0600

    Creating SearchRectangle and Options

--- src/main/java/com/prototest/solanum/SearchOptions.java
+++ src/main/java/com/prototest/solanum/SearchOptions.java
@@ -1,12 +1,107 @@
 package com.prototest.solanum;
 
+import java.awt.*;
+
 /**
  * Created by Brian on 5/12/2014.
  */
 public class SearchOptions {
 
+    public String imageName;
+    public Point hotSpot;
+    public String searchType;
+    public Integer tolerance;
+    public Integer discrepancy;
+    public Boolean pulsing;
+    public String collectionFilter;
+    public Rectangle clipRectangle;
+    public Rectangle searchRectangle;
 
-    public String getString() {
-        return "";
+    public String text;
+    public String textStyle;
+    public Boolean caseSensitive;
+    public Boolean contrast;
+    public String contrastColor;
+    public Boolean contrastTolerance;
+    public Integer dpi;
+    public Boolean ignoreSpaces;
+    public Boolean ignoreUnderscores;
+    public String language;
+    public String validCharacters;
+
+    public String getImageString() {
+        String result = "";
+        if(hotSpot!=null){
+            result+= String.format(", HotSpot: (%s,%s)",hotSpot.getX(),hotSpot.getY());
+        }
+        if(searchType!=null){
+            result+= String.format(", SearchType: \"%s\"",searchType);
+        }
+        if(tolerance!=null){
+            result+= String.format(", Tolerance: %s",tolerance);
+        }
+        if(discrepancy!=null){
+            result+= String.format(", Discrepancy: %s",discrepancy);
+        }
+        if(pulsing!=null){
+            result+= String.format(", Pulsing: %s",pulsing);
+        }
+        if(collectionFilter!=null){
+            result+= String.format(", CollectionFilter: (Name: \"%s\")",collectionFilter);
+        }
+
+        if(clipRectangle==null){
+            result+= String.format(", ClipRectangle: (%s,%s,%s,%s)",clipRectangle.getX(),clipRectangle.getY(),clipRectangle.getX()+clipRectangle.getWidth(),clipRectangle.getY()+clipRectangle.getHeight());
+        }
+        if(collectionFilter==null){
+            result+= String.format(",SearchRectangle: (%s,%s,%s,%s)",searchRectangle.getX(),searchRectangle.getY(),searchRectangle.getX()+searchRectangle.getWidth(),searchRectangle.getY()+searchRectangle.getHeight());
+        }
+        return result;
     }
+
+    public String getTextString() {
+        String result = "";
+
+        if(textStyle!=null){
+            result+= String.format(", TextStyle: \"%s\"",textStyle);
+        }
+        if(caseSensitive!=null){
+            result+= String.format(", CaseSensitive: %s",caseSensitive);
+        }
+        if(contrast!=null){
+            result+= String.format(", Contrast: %s",contrast);
+        }
+        if(contrastColor!=null){
+            result+= String.format(", ContrastColor: \"%s\"",contrastColor);
+        }
+        if(contrastTolerance!=null){
+            result+= String.format(", ContrastTolerance: %s",contrastTolerance);
+        }
+        if(dpi!=null){
+            result+= String.format(", DPI: %s",dpi);
+        }
+        if(ignoreSpaces!=null){
+            result+= String.format(", IgnoreSpaces: %s",ignoreSpaces);
+        }
+        if(ignoreUnderscores!=null){
+            result+= String.format(", IgnoreUnderscores: %s",ignoreUnderscores);
+        }
+        if(language!=null){
+            result+= String.format(", Language: \"%s\"",ignoreSpaces);
+        }
+        if(validCharacters!=null){
+            result+= String.format(", ValidCharacters: \"%s\"",ignoreSpaces);
+        }
+        return result;
+    }
+
+    public SearchOptions(String imageName){
+        this.imageName = imageName;
+    }
+    public SearchOptions(String imageName, Rectangle searchRectangle){
+        this.imageName = imageName;
+        this.searchRectangle = searchRectangle;
+    }
+
+
 }
--- src/main/java/com/prototest/solanum/SearchRectangle.java
+++ src/main/java/com/prototest/solanum/SearchRectangle.java
@@ -1,7 +1,68 @@
 package com.prototest.solanum;
 
+import com.sun.javafx.css.Size;
+
+import java.awt.*;
+
 /**
  * Created by Brian on 5/12/2014.
  */
 public class SearchRectangle {
+    public Rectangle searchRectangle = null;
+    public Point upperLeft;
+    public Point lowerRight;
+    public int width;
+    public int height;
+    public static String xPoint;
+    public static String yPoint;
+    public static SearchRectangle fullScreen;
+
+
+    public SearchRectangle(Rectangle rectangle)
+    {
+        this.searchRectangle = rectangle;
+        this.upperLeft = rectangle.getLocation();
+        this.width = upperLeft.x - lowerRight.y;
+        this.height = upperLeft.x - lowerRight.y;
+        this.lowerRight = new Point(upperLeft.x+width,upperLeft.y+height);
+        //this.fullScreen = EggplantTestBase.driver.getScreenRectangle();
+    }
+
+    public SearchRectangle(Point upperLeft, Point lowerRight)
+    {
+        this.upperLeft = upperLeft;
+        this.lowerRight = lowerRight;
+        this.width = Math.abs(upperLeft.x - lowerRight.x);
+        this.height = Math.abs(upperLeft.y - lowerRight.y);
+        this.searchRectangle = new Rectangle(upperLeft.x,upperLeft.y,this.width,this.height);
+        //this.fullScreen = EggplantTestBase.driver.getScreenRectangle();
+    }
+
+    public static SearchRectangle topHalf()
+    {
+        return new SearchRectangle(new Point(0,0),new Point(fullScreen.width,fullScreen.height/2));
+    }
+
+    public static SearchRectangle bottomHalf()
+    {
+        return new SearchRectangle(new Point(0, fullScreen.height/2), new Point(fullScreen.width, fullScreen.height));
+    }
+
+    public static SearchRectangle middleHalf()
+    {
+            int value = (int) Math.floor(fullScreen.height*.25);
+            return new SearchRectangle(new Point(0, value), new Point(fullScreen.width, (int)Math.floor(fullScreen.height * .75)));
+    }
+
+    public static SearchRectangle topQuarter()
+    {
+       return new SearchRectangle(new Point(0, 0), new Point(fullScreen.width, fullScreen.height / 4));
+    }
+
+    public static SearchRectangle bottomQuarter()
+    {
+        int value = (int) Math.floor(fullScreen.height*.75);
+        return new SearchRectangle(new Point(0, value), new Point(fullScreen.width, fullScreen.height));
+
+    }
 }
