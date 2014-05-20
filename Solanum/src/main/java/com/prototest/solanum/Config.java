7395f11078a3050bde665c1df3080ef6f422a735
andar <andartagg@gmail.com>
2014-05-20 14:05:29 -0600

    Config changes: load the config file only once; allow config location to be changed at runtime; allow config values to be read from the command line

--- Solanum/src/main/java/com/prototest/solanum/Config.java
+++ Solanum/src/main/java/com/prototest/solanum/Config.java
@@ -7,59 +7,71 @@ import java.util.Properties;
  * Created by Brian on 5/12/2014.
  */
 public class Config {
-    private static Properties properties = new Properties();
-    private static InputStream input = null;
+    private static Properties properties;
     private static OutputStream output = null;
-    public static boolean captureScreenshots = getPropertyValue("captureScreenshotsOnError",true);
-    public static boolean startDrive = getPropertyValue("startDrive",true);
-    public static String eggplantPath = getPropertyValue("eggplantPath","");
-    public static String runScriptPath = getPropertyValue("runScriptPath","C:\\Program Files (x86)\\eggPlant\\runscript.bat");
+    public static boolean captureScreenshots = getPropertyValue("captureScreenshotsOnError", true);
+    public static boolean startDrive = getPropertyValue("startDrive", true);
+    public static String eggplantPath = getPropertyValue("eggplantPath", "");
+    public static String runScriptPath = getPropertyValue("runScriptPath", "C:\\Program Files (x86)\\eggPlant\\runscript.bat");
     public static String currentPath = System.getProperty("user.dir");
     public static String suitePath = getPropertyValue("suitePath", "");
-    public static String drivePort = getPropertyValue("drivePort","5400");
+    public static String drivePort = getPropertyValue("drivePort", "5400");
     public static String driveLoggingLevel = getPropertyValue("driveLoggingLevel", "2");
-    public static boolean logDriveCommands = getPropertyValue("logDriveCommands",true);
-    public static int commandDelayMs = getPropertyValue("commandDelayMs",1000);
-    public static int elementWaitTimeSec = getPropertyValue("elementWaitTimeSec",5);
+    public static boolean logDriveCommands = getPropertyValue("logDriveCommands", true);
+    public static int commandDelayMs = getPropertyValue("commandDelayMs", 1000);
+    public static int elementWaitTimeSec = getPropertyValue("elementWaitTimeSec", 5);
     // TODO does clickExecuteDelay duplicate the intention of any of these other settings?
     // TODO Yes it does it duplicates commandDelayMs
     public static int clickExecuteDelay = getPropertyValue("clickExecuteDelay", 1000);
     public static String hostName = getPropertyValue("hostName", "localhost");
     public static int hostPort = getPropertyValue("hostPort", 5900);
-    public static String imageSearchTime = getPropertyValue("imageSearchTime","1");
-    public static String imageSearchCount = getPropertyValue("imageSearchCount","1");
-    public static String driveUrl = getPropertyValue("driveUrl","http://127.0.0.1:5400");
+    public static String imageSearchTime = getPropertyValue("imageSearchTime", "1");
+    public static String imageSearchCount = getPropertyValue("imageSearchCount", "1");
+    public static String driveUrl = getPropertyValue("driveUrl", "http://127.0.0.1:5400");
 
-    private static int getPropertyValue(String key,int defaultValue){
-        String result = getPropertyValue(key,null);
-        if(result==null) return defaultValue;
+    private static int getPropertyValue(String key, int defaultValue) {
+        String result = getPropertyValue(key, null);
+        if (result == null) return defaultValue;
         return Integer.parseInt(result);
     }
 
+    private static void init() {
+        properties = new Properties(System.getProperties());
+
+        String configLocation = System.getProperty("configFile");
+        InputStream input = null;
+        if (configLocation != null) {
+            try {
+                input = new FileInputStream(configLocation);
+            } catch (FileNotFoundException e) {
+                Logger.warning("Config file " + configLocation + " not found. Using defaults.");
+            }
+        } else {
+            input = ClassLoader.getSystemClassLoader().getResourceAsStream("config.properties");
+        }
+        if (input == null) {
+            Logger.message("Config file not found, using defaults.");
+        } else {
+            try {
+                properties.load(input);
+            } catch (IOException e) {
+                Logger.error("Could not load config file. " + e.getMessage());
+            }
+        }
+    }
 
     public static String getPropertyValue(String key, String defaultValue) {
-        try {
-            InputStream input = ClassLoader.getSystemClassLoader().getResourceAsStream("config.properties");
-            properties.load(input);
-            String result = properties.getProperty(key, defaultValue);
-            return result;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return defaultValue;
+        if (properties == null) init();
+
+        return properties.getProperty(key, defaultValue);
     }
 
     public static boolean getPropertyValue(String key, boolean defaultValue) {
+        if (properties == null) init();
 
-        try {
-            String result = getPropertyValue(key,null);
-            if(result==null) return defaultValue;
-            return isTruthy(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-           return defaultValue;
-
+        String result = getPropertyValue(key, null);
+        if (result == null) return defaultValue;
+        return isTruthy(result);
     }
 
     public static void setPropertyValue(String key, String value) {
@@ -74,7 +86,7 @@ public class Config {
         }
     }
 
-    private static boolean isTruthy(String value){
+    private static boolean isTruthy(String value) {
         return value.equals("True") || value.equals("true");
     }
 }
