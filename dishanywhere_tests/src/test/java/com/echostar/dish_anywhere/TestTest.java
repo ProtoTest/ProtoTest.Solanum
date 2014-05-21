7be80d1ea9ffd2d88401e90dc1edf55ac076c3a1
andrewtaggart <andrew.taggart@colorado.edu>
2014-05-21 16:20:10 -0600

    Add login test POC.

--- dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/TestTest.java
+++ dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/TestTest.java
@@ -1,7 +1,10 @@
 package com.echostar.dish_anywhere;
 
 import com.echostar.dish_anywhere.eggplant_suite.androidphone.AndroidScreen;
+import com.echostar.dish_anywhere.eggplant_suite.androidphone.DishAnywhereHome;
+import com.echostar.dish_anywhere.eggplant_suite.androidphone.DishAnywhereLogin;
 import com.echostar.dish_anywhere.eggplant_suite.androidphone.HomeScreen;
+import com.prototest.solanum.By;
 import com.prototest.solanum.EggplantTestBase;
 import com.prototest.solanum.SearchRectangle;
 import com.prototest.solanum.TextOption;
@@ -17,5 +20,14 @@ public class TestTest extends EggplantTestBase {
         homeScreen.nav.homeButton.click();
         AndroidScreen screen = homeScreen.openApp("eggOn", TextOption.searchRectangle(SearchRectangle.bottomHalf()));
         //screen.clickOn("Stop eggOn", TextOption.searchRectangle(SearchRectangle.bottomHalf()));
+    }
+
+    @Test
+    public void loginTest() {
+        DishAnywhereHome home = new AndroidScreen()
+                .goHome().openApp("Anywhere", DishAnywhereHome.class, TextOption.searchRectangle(SearchRectangle.bottomHalf()));
+        DishAnywhereLogin login = home.openSettings().logout();
+        login.login("guest1full", "guest4321");
+
     }
 }
---
+++ dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/eggplant_suite/androidphone/DishAnywhereLogin.java
@@ -0,0 +1,33 @@
+package com.echostar.dish_anywhere.eggplant_suite.androidphone;
+
+import com.prototest.solanum.By;
+import com.prototest.solanum.EggplantElement;
+import com.prototest.solanum.SearchRectangle;
+import com.prototest.solanum.TextOption;
+import org.testng.Assert;
+
+import java.awt.*;
+
+/**
+ */
+public class DishAnywhereLogin extends AndroidScreen {
+    private EggplantElement onlineId = new EggplantElement(By.Text("Online ID", TextOption.hotSpot(new Point(75,109))));
+    private EggplantElement password = new EggplantElement(By.Text("Password", TextOption.hotSpot(new Point(75,109)), TextOption.searchRectangle(SearchRectangle.middleHalf())));
+    private EggplantElement loginButton = new EggplantElement(By.Image("AndroidPhone/GalaxyS5/Apps/DishAnywhere/Login/LoginButton"));
+    public DishAnywhereLogin() {
+        super();
+    }
+
+    public DishAnywhereHome login(String onlineId, String password) {
+        this.onlineId.type(onlineId);
+        this.password.type(password);
+        loginButton.click();
+        EggplantElement errorField = new EggplantElement(By.Text("The Online ID / Password combination", SearchRectangle.topHalf()));
+        if (errorField.isPresent()) {
+            Assert.fail("Didn't login");
+        }
+        return new DishAnywhereHome();
+    }
+
+
+}
