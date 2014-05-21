21275f710b48a0565bf6d784ca3314a9e5d68ff9
bkitchener1 <kitchener.brian@gmail.com>
2014-05-21 17:18:17 -0600

    adding

--- Solanum/src/main/java/com/prototest/solanum/EggplantResponse.java
+++ Solanum/src/main/java/com/prototest/solanum/EggplantResponse.java
@@ -1,7 +1,22 @@
 package com.prototest.solanum;
 
+import java.io.Serializable;
+import java.security.KeyPair;
+import java.util.HashMap;
+
 /**
  * Created by Brian on 5/21/2014.
  */
 public class EggplantResponse {
+    public EggplantResponse(HashMap<String, String> value){
+        this.Output = value.get("Output;");
+        this.Result = value.get("Result");
+        this.Duration = value.get("Duration");
+        this.ReturnValue = value.get("ReturnValue");
+    }
+    public final String Output;
+    public final String Result;
+    public final String Duration;
+    public final String ReturnValue;
+
 }
