e9fd846d8167fcc2d7d2d7737c7fb94ae971d244
andrewtaggart <andrew.taggart@colorado.edu>
2014-05-23 11:26:09 -0600

    Cache the screen rectangle.

--- Solanum/src/main/java/com/prototest/solanum/EggplantDriver.java
+++ Solanum/src/main/java/com/prototest/solanum/EggplantDriver.java
@@ -14,25 +14,44 @@ import java.util.regex.Pattern;
 public class EggplantDriver {
 
     private EggplantDriveClient client;
+    private Rectangle screenRectangle;
+    private Point screenSize;
 
     public EggplantDriver() {
         this.client = new EggplantDriveClient();
     }
 
     public void connect() {
-        connect(Config.hostName,Config.hostPort);
+        connect(Config.hostName, Config.hostPort);
     }
 
     public void connect(String host) {
         execute(String.format("Connect (ServerID:\"%s\")", host));
+        initProperties();
     }
 
     public void connect(String host, int portNum) {
         execute(String.format("Connect (ServerID:\"%s\", portNum: \"%s\")", host, portNum));
+        initProperties();
+    }
+
+    private void initProperties() {
+        String screenRectString = execute("put RemoteScreenRectangle()").Output;
+        screenRectString = screenRectString.substring(1, screenRectString.length() - 1);
+        String[] rect = screenRectString.split(",");
+        this.screenRectangle = new Rectangle(Integer.parseInt(rect[0]),
+                Integer.parseInt(rect[1]),
+                Integer.parseInt(rect[2]),
+                Integer.parseInt(rect[3]));
+
+        String screenSizeString = execute("put RemoteScreenSize()").Output;
+        screenSizeString = screenSizeString.substring(1, screenSizeString.length() - 1);
+        String[] screenSizeRect = screenSizeString.split(",");
+        screenSize = new Point(Integer.parseInt(screenSizeRect[0]), Integer.parseInt(screenSizeRect[1]));
     }
 
     public void disconnect() {
-        disconnect(Config.hostName,Config.hostPort);
+        disconnect(Config.hostName, Config.hostPort);
 
     }
 
@@ -69,141 +88,118 @@ public class EggplantDriver {
     }
 
 
-    public void endSuite(){
-        if(Config.logDriveCommands){
+    public void endSuite() {
+        if (Config.logDriveCommands) {
             Logger.message("Ending Current Suite");
         }
         client.endSession();
     }
 
-    public EggplantElement findElement(By by)
-    {
-       return findElement(by.getLocator());
+    public EggplantElement findElement(By by) {
+        return findElement(by.getLocator());
     }
-    public EggplantElement findElement(String locator)
-    {
+
+    public EggplantElement findElement(String locator) {
         String output = execute(String.format("Put ImageLocation %s", locator)).Output;
-        output = output.substring(1,output.length()-1);
+        output = output.substring(1, output.length() - 1);
         String[] rect = output.split(",");
-        Point point = new Point(Integer.parseInt(rect[0]),Integer.parseInt(rect[1]));
+        Point point = new Point(Integer.parseInt(rect[0]), Integer.parseInt(rect[1]));
         return new EggplantElement(By.Point(point));
     }
-    public void click(String locator)
-    {
+
+    public void click(String locator) {
         execute(String.format("Click %s", locator));
     }
 
-    public void tap(String locator)
-    {
+    public void tap(String locator) {
         execute(String.format("Tap %s", locator));
     }
 
-    public void doubleTap(String locator)
-    {
+    public void doubleTap(String locator) {
         execute(String.format("DoubleTap %s", locator));
     }
 
-    public void press(String locator)
-    {
+    public void press(String locator) {
         execute(String.format("Press %s", locator));
     }
 
-    public void release(String locator)
-    {
+    public void release(String locator) {
         execute(String.format("Release %s", locator));
     }
 
-    public void drag(String locator)
-    {
+    public void drag(String locator) {
         execute(String.format("Drag %s", locator));
     }
 
-    public void drop(String locator)
-    {
+    public void drop(String locator) {
         execute(String.format("Drop %s", locator));
     }
 
-    public void dragAndDrop(String fromlocator, String tolocator)
-    {
+    public void dragAndDrop(String fromlocator, String tolocator) {
         drag(fromlocator);
         drop(tolocator);
     }
 
-    public void waitFor(String locator, String timeoutSec)
-    {
-        execute(String.format("WaitFor %s, %s",locator,timeoutSec));
+    public void waitFor(String locator, String timeoutSec) {
+        execute(String.format("WaitFor %s, %s", locator, timeoutSec));
     }
 
-    public void rightClick(String locator)
-    {
+    public void rightClick(String locator) {
         execute(String.format("RightClick ", locator));
     }
 
-    public boolean isPresent(String locator)
-    {
-       return execute(String.format("put ImageFound %s", locator)).Output.contains("True");
+    public boolean isPresent(String locator) {
+        return execute(String.format("put ImageFound %s", locator)).Output.contains("True");
     }
 
-    public void typeText(String text)
-    {
+    public void typeText(String text) {
         execute(String.format("TypeText \"%s\"", text));
     }
 
-    public void scrollWheelUp(String num)
-    {
+    public void scrollWheelUp(String num) {
         execute(String.format("ScrollWheelUp %s", num));
     }
 
-    public void scrollWheelDown(String num)
-    {
+    public void scrollWheelDown(String num) {
         execute(String.format("ScrollWheelDown %s", num));
     }
 
-    public void swipeDown(Point origin)
-    {
-        execute(String.format("SwipeDown (%s,%s)",origin.x,origin.y));
+    public void swipeDown(Point origin) {
+        execute(String.format("SwipeDown (%s,%s)", origin.x, origin.y));
     }
 
-    public void swipeDown(String locator)
-    {
+    public void swipeDown(String locator) {
         execute(String.format("SwipeDown %s", locator));
     }
 
-    public void swipeUp(String locator)
-    {
+    public void swipeUp(String locator) {
         execute(String.format("SwipeUp %s", locator));
     }
 
 
-    public void swipeRight(String locator)
-    {
+    public void swipeRight(String locator) {
         execute(String.format("SwipeRight %s", locator));
     }
 
-    public void swipeLeft(String locator)
-    {
+    public void swipeLeft(String locator) {
         execute(String.format("SwipeLeft %s", locator));
     }
-    public void swipeUp(Point origin)
-    {
-        execute(String.format("SwipeUp (%s,%s)",origin.x,origin.y));
+
+    public void swipeUp(Point origin) {
+        execute(String.format("SwipeUp (%s,%s)", origin.x, origin.y));
     }
 
-    public void swipeLeft(Point origin)
-    {
-        execute(String.format("SwipeLeft (%s,%s)",origin.x,origin.y));
+    public void swipeLeft(Point origin) {
+        execute(String.format("SwipeLeft (%s,%s)", origin.x, origin.y));
     }
 
-    public void swipeRight(Point origin)
-    {
-        execute(String.format("SwipeDown (%s,%s)",origin.x,origin.y));
+    public void swipeRight(Point origin) {
+        execute(String.format("SwipeDown (%s,%s)", origin.x, origin.y));
     }
 
 
-    public String getScreenshot()
-    {
-        try
-        {
+    public String getScreenshot() {
+        try {
             java.util.Date date = new java.util.Date();
             SimpleDateFormat sdf = new SimpleDateFormat("MMddyyyyhhmmss");
             String timestamp = sdf.format(date);
@@ -213,66 +209,47 @@ public class EggplantDriver {
             String path = Config.currentPath + separator + "Screenshots" + separator + timestamp + ".tiff";
             captureScreenshot(path);
             return path;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Logger.warning("Error capturing image : " + e.getMessage());
         }
         return null;
     }
 
-    public void captureScreenshot(String path){
-        execute(String.format("CaptureScreen (Name: \"%s\")",path ));
+    public void captureScreenshot(String path) {
+        execute(String.format("CaptureScreen (Name: \"%s\")", path));
     }
 
-    public String readText(String locator)
-    {
+    public String readText(String locator) {
         //TODO this needs to be tested, does the data return in the Result or Output key?
         String result = execute(String.format("put ReadText %s" + locator)).Output;
-       return result;
+        return result;
     }
 
-    public Rectangle getScreenRectangle()
-    {
-        String output = execute("put RemoteScreenRectangle()").Output;
-        output = output.substring(1,output.length()-1);
-        String[] rect = output.split(",");
 
-        return new Rectangle(Integer.parseInt(rect[0]),Integer.parseInt(rect[1]),Integer.parseInt(rect[2]),Integer.parseInt(rect[3]));
+
+    public Point getScreenSize() {
+        return screenSize;
     }
 
-    public Point getScreenSize()
-    {
-        String output = execute("put RemoteScreenSize()").Output;
-        output = output.substring(1,output.length()-1);
-        String[] rect = output.split(",");
-
-        return new Point(Integer.parseInt(rect[0]),Integer.parseInt(rect[1]));
-    }
-
-    public String getConnectionInfo()
-    {
+    public String getConnectionInfo() {
         return (String) execute("put ConnectionInfo()").Output;
     }
 
-    public String getOptions()
-    {
+    public String getOptions() {
         String output = execute("put getOptions()").Output;
         return output;
     }
 
-    public String getOption(String option)
-    {
-        String output = (String) execute(String.format("put getOption(%s)",option)).Output;
+    public String getOption(String option) {
+        String output = (String) execute(String.format("put getOption(%s)", option)).Output;
         return output;
     }
 
-    public void setOption(String option,String value)
-    {
-        execute(String.format("set the %s to %s",option,value));
+    public void setOption(String option, String value) {
+        execute(String.format("set the %s to %s", option, value));
     }
 
-    public boolean isDriveRunning(){
+    public boolean isDriveRunning() {
         try {
             getOptions();
             return true;
@@ -281,13 +258,16 @@ public class EggplantDriver {
         }
     }
 
-    public Rectangle getImageRectangle(String locator){
-        String output = execute(String.format("Put ImageRectangle %s",locator)).Output;
-        output = output.substring(1,output.length()-1);
+    public Rectangle getImageRectangle(String locator) {
+        String output = execute(String.format("Put ImageRectangle %s", locator)).Output;
+        output = output.substring(1, output.length() - 1);
         String[] rect = output.split(",");
-        return new Rectangle(Integer.parseInt(rect[0]),Integer.parseInt(rect[1]),Integer.parseInt(rect[2]),Integer.parseInt(rect[3]));
+        return new Rectangle(Integer.parseInt(rect[0]), Integer.parseInt(rect[1]), Integer.parseInt(rect[2]), Integer.parseInt(rect[3]));
 
     }
 
 
+    public Rectangle getScreenRectangle() {
+        return screenRectangle;
+    }
 }
