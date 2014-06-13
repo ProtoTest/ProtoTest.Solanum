5d6ab294e029dc7e92f2528bbd1c86cc448e8be2
andrewtaggart <andrew.taggart@colorado.edu>
2014-06-13 09:04:00 -0600

    Add galaxy tab blockbuster tests.

--- dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/aTablet/galaxyNote/GalaxyNoteBlockbuster.java
+++ dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/aTablet/galaxyNote/GalaxyNoteBlockbuster.java
@@ -1,7 +1,93 @@
 package com.echostar.dish_anywhere.tests.aTablet.galaxyNote;
 
+import com.echostar.dish_anywhere.radish.RadishScraper;
+import com.echostar.dish_anywhere.screenobjects.aTablet.galaxyNote.DishAnywhereHome;
+import com.prototest.solanum.Verifications;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Brian on 6/4/2014.
  */
 public class GalaxyNoteBlockbuster {
+
+    private final int MOVIES_TO_TEST = 6;
+
+    @Test
+    public void movieCategory(){
+
+        RadishScraper radishScraper = new RadishScraper();
+        List<Map<String, String>> movies = radishScraper.getBlockbusterMoviesCategory();
+
+        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
+
+        for (int i = 0; i < MOVIES_TO_TEST; i++) {
+            movieTitles.add(movies.get(i).get("franchiseName"));
+        }
+        new DishAnywhereHome()
+                .openDishAnywhereHome()
+                .openBlockbuster()
+                .openMovies()
+                .verifyTitlesPresent(movieTitles);
+    }
+
+    @Test
+    public void tvShowsCategory(){
+
+        RadishScraper radishScraper = new RadishScraper();
+        List<Map<String, String>> movies = radishScraper.getBlockbusterShowsCategory();
+
+        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
+
+        for (int i = 0; i < MOVIES_TO_TEST; i++) {
+            movieTitles.add(movies.get(i).get("franchiseName"));
+        }
+        new DishAnywhereHome()
+                .openDishAnywhereHome()
+                .openBlockbuster()
+                .openMovies()
+                .verifyTitlesPresent(movieTitles);
+        Verifications.assertVerifications();
+    }
+
+    @Test
+    public void kidsMovieCategory(){
+
+        RadishScraper radishScraper = new RadishScraper();
+        List<Map<String, String>> movies = radishScraper.getBlockbusterKidsMoviesCategory();
+
+        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
+
+        for (int i = 0; i < MOVIES_TO_TEST; i++) {
+            movieTitles.add(movies.get(i).get("franchiseName"));
+        }
+        new DishAnywhereHome()
+                .openDishAnywhereHome()
+                .openBlockbuster()
+                .openTVShows()
+                .verifyTitlesPresent(movieTitles);
+        Verifications.assertVerifications();
+    }
+
+    @Test
+    public void kidsTvShowsCategory(){
+
+        RadishScraper radishScraper = new RadishScraper();
+        List<Map<String, String>> movies = radishScraper.getBlockbusterKidsShowsCategory();
+
+        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
+
+        for (int i = 0; i < MOVIES_TO_TEST; i++) {
+            movieTitles.add(movies.get(i).get("franchiseName"));
+        }
+        new DishAnywhereHome()
+                .openDishAnywhereHome()
+                .openBlockbuster()
+                .openMovies()
+                .verifyTitlesPresent(movieTitles);
+        Verifications.assertVerifications();
+    }
 }
