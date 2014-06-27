04a1dc19a77b290777084a7715a25964876e06a1
andrewtaggart <andrew.taggart@colorado.edu>
2014-06-27 13:17:26 -0600

    Add By doc.

--- Solanum/src/main/java/com/prototest/solanum/By.java
+++ Solanum/src/main/java/com/prototest/solanum/By.java
@@ -5,9 +5,12 @@ import java.io.File;
 
 /**
  *
- * The By class creates an eggplant locator string in the correct format.  It accepts an optional list of search options.
- * Three ways to define an element.  Using an image (image: "/path/to/image"), a text value (text: "value") or a point (12,14).
- * It is designed to be passed into the EggplantElement class to allow easy object instantiation.
+ * <pre>By</pre> is used to describe how to find an element on screen..  It accepts an optional list of search options.
+ * Three ways to define an element.  Using an image ({@link By#Image(String, ImageOption...)}),
+ * a text value ({@link By#Text(String, SearchRectangle, TextOption...)}),
+ * or a point ({@link By#Point(java.awt.Point)}).
+ *
+ * @see EggplantElement
  */
 public class By {
     public final ByType type;
@@ -23,7 +26,7 @@ public class By {
         return locator;
     }
 
-    public By(String locator, ByType type) {
+    private By(String locator, ByType type) {
         this.locator = locator;
         this.searchRectangle = new SearchRectangle(EggplantTestBase.driver.getScreenRectangle());
         this.type = type;
@@ -46,10 +49,23 @@ public class By {
         return image;
     }
 
+    /**
+     * Create a by using an image from the EggPlant test suite. Use a path relative to the {@link Config} suitePath.
+     * @param path Path to the image asset. Can either refer to a specific file, or to an EggPlant image collection.
+     * @param options Optional parameters to control search and discovery of the element.
+     * @return The constructed {@link By}.
+     */
     public static By Image(String path, ImageOption... options) {
         return Image(path, null, options);
     }
 
+    /**
+     * Create a by using an image from the EggPlant test suite. Use a path relative to the {@link Config} suitePath.
+     * @param path Path to the image asset. Can either refer to a specific file, or to an EggPlant image collection.
+     * @param searchRectangle The on-screen {@link SearchRectangle} to limit searching to.
+     * @param options Optional parameters to control search and discovery of the element.
+     * @return The constructed {@link By}.
+     */
     public static By Image(String path, SearchRectangle searchRectangle, ImageOption... options) {
         String locatorString = String.format("(image: \"%s\"", path);
         if (searchRectangle != null) {
@@ -73,10 +89,22 @@ public class By {
         return new By(locatorString, searchRectangle, ByType.image,file);
     }
 
+    /**
+     * Create a {@link By} using text. When used in an {@link EggplantElement}, Solanum uses EggPlant's OCR capability to locate the text.
+     * @param text Text to search for.
+     * @param options Optional parameters to control search and discovery of the element.
+     * @return The constructed {@link By}.
+     */
     public static By Text(String text, TextOption... options) {
         return Text(text, null, options);
     }
-
+    /**
+     * Create a {@link By} using text. When used in an {@link EggplantElement}, Solanum uses EggPlant's OCR capability to locate the text.
+     * @param text Text to search for.
+     * @param searchRectangle The on-screen {@link SearchRectangle} to limit searching to.
+     * @param options Optional parameters to control search and discovery of the element.
+     * @return The constructed {@link By}.
+     */
     public static By Text(String text, SearchRectangle searchRectangle, TextOption... options) {
         String locatorString = String.format("(text: \"%s\"", text);
         if (searchRectangle != null) {
@@ -89,6 +117,11 @@ public class By {
         return new By(locatorString, searchRectangle, ByType.text);
     }
 
+    /**
+     * Create a {@link By} which references an on-screen pixel location.
+     * @param point The on-screen pixel location.
+     * @return The constructed {@link By}.
+     */
     public static By Point(Point point) {
         String locatorString = String.format("(%s,%s)", point.getX(), point.getY());
         return new By(locatorString, ByType.point);
