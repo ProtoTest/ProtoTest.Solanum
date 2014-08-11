eba86ad2dd9291fc4dfa2e407496386d559508a6
Andrew Taggart <ataggart@prototest.com>
2014-08-11 00:21:56 -0600

    Initialize only once per test run.

--- dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/kindleTablet/KindleFireHDX/KindleTestBase.java
+++ dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/kindleTablet/KindleFireHDX/KindleTestBase.java
@@ -3,6 +3,7 @@ package com.echostar.dish_anywhere.tests.kindleTablet.KindleFireHDX;
 import com.echostar.dish_anywhere.screenobjects.kindleTablet.kindleFire.DeviceMain;
 import com.prototest.solanum.Config;
 import com.prototest.solanum.EggplantTestBase;
+import com.prototest.solanum.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 
@@ -10,20 +11,38 @@ import org.testng.annotations.BeforeTest;
  */
 public class KindleTestBase extends EggplantTestBase {
 
+    // Only initialize if this alreadyInitialized is false.
+    // This isn't thread safe, but the expected use case is single threaded execution on one Kindle device.
+
+    static boolean alreadyInitialized = false;
+
     @BeforeTest
     public void clearAppState() {
-        new DeviceMain()
-                .goHome()
-                .logOutIfLoggedIn()
-                .login(Config.getTestProp("dishAnywhereLoginName"), Config.getTestProp("dishAnywhereLoginPass"))
-                .openSettings()
-                .openAuthorizedDevices()
-                .authorizeThisDevice()
-                .openParentalControls(Config.getTestProp("dishAnywherePassCode"))
-                .clearMovieBlocks()
-                .clearTVBlocks()
-                .save()
-                .openGuide();
+        // If the app ever crashes, the app should be initialized even if alreadyInitialized is true.
+        if (!alreadyInitialized /* || isCrashed() */) {
+            /*
+            if (isCrashed()) {
+                clearCrash();
+            }
+             */
+            new DeviceMain()
+                    .resetApp()
+                    .logOutIfLoggedIn()
+                    .login(Config.getTestProp("dishAnywhereLoginName"), Config.getTestProp("dishAnywhereLoginPass"))
+                    .openSettings()
+                    .openAuthorizedDevices()
+                    .authorizeThisDevice()
+                    .openParentalControls(Config.getTestProp("dishAnywherePassCode"))
+                    .clearMovieBlocks()
+                    .clearTVBlocks()
+                    .save()
+                    .openGuide();
+            alreadyInitialized = true;
+        } else {
+            Logger.info("KindleTestBase already initialized; not re-running setup");
+            new DeviceMain()
+                    .goHome();
+        }
     }
 
     @AfterMethod
