5fda78f02e6657ea8106df338f8dd6aef82e294d
Andrew Taggart <ataggart@prototest.com>
2014-08-13 20:01:31 -0600

    Comment broken and unused placeholder code for new kindle app version

--- dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/KindleFireHDX_updatedversion/KindleFireParentalControls.java
+++ dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/KindleFireHDX_updatedversion/KindleFireParentalControls.java
@@ -1,57 +1,57 @@
-package com.echostar.dish_anywhere.tests.KindleFireHDX_updatedversion;
-
-import com.echostar.dish_anywhere.radish.RadishScraper;
-import com.echostar.dish_anywhere.screenobjects.kindleTablet.kindleFire.DishAnywhereHome;
-import org.testng.annotations.Test;
-
-import java.util.List;
-import java.util.Map;
-
-/**
- * Created by Brian on 6/4/2014.
- */
-public class KindleFireParentalControls extends KindleTestBase {
-    @Test
-    public void clearAllBlockedContentAndPlayMovie(){
-
-        RadishScraper radishScraper = new RadishScraper();
-        List<Map<String, String>> movies = radishScraper.getMoviesCategory(RadishScraper.Device.android_phone, 19);
-
-        String movieName = movies.get(0).get("franchiseName");
-        new DishAnywhereHome()
-                .openSettings()
-                .openParentalControls("1111")
-                .clearMovieBlocks()
-                .clearTVBlocks()
-                .save()
-                .openOnDemand()
-                .searchFor(movieName)
-                .openOnDemandResults()
-                .openMovie(movieName)
-                .watchMovie()
-                .verifyMoviePlays();
-    }
-
-    @Test
-    public void setAllContentBlockedAndPlayMovie(){
-
-        RadishScraper radishScraper = new RadishScraper();
-        List<Map<String, String>> movies = radishScraper.getMoviesCategory(RadishScraper.Device.android_phone, 19);
-
-        String movieName = movies.get(0).get("franchiseName");
-        new DishAnywhereHome()
-                .openSettings()
-                .openParentalControls("1111")
-                .setMovieGBlocked()
-                .setTVYBlocked()
-                .save()
-                .openOnDemand()
-                .searchFor(movieName)
-
-                .openProtectedMovie(movieName)
-                .enterPasscode("1111")
-                .watchMovie()
-                .verifyMoviePlays();
-
-    }
-}
+//package com.echostar.dish_anywhere.tests.KindleFireHDX_updatedversion;
+//
+//import com.echostar.dish_anywhere.radish.RadishScraper;
+//import com.echostar.dish_anywhere.screenobjects.kindleTablet.kindleFire.DishAnywhereHome;
+//import org.testng.annotations.Test;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * Created by Brian on 6/4/2014.
+// */
+//public class KindleFireParentalControls extends KindleTestBase {
+//    @Test
+//    public void clearAllBlockedContentAndPlayMovie(){
+//
+//        RadishScraper radishScraper = new RadishScraper();
+//        List<Map<String, String>> movies = radishScraper.getMoviesCategory(RadishScraper.Device.android_phone, 19);
+//
+//        String movieName = movies.get(0).get("franchiseName");
+//        new DishAnywhereHome()
+//                .openSettings()
+//                .openParentalControls("1111")
+//                .clearMovieBlocks()
+//                .clearTVBlocks()
+//                .save()
+//                .openOnDemand()
+//                .searchFor(movieName)
+//                .openOnDemandResults()
+//                .openMovie(movieName)
+//                .watchMovie()
+//                .verifyMoviePlays();
+//    }
+//
+//    @Test
+//    public void setAllContentBlockedAndPlayMovie(){
+//
+//        RadishScraper radishScraper = new RadishScraper();
+//        List<Map<String, String>> movies = radishScraper.getMoviesCategory(RadishScraper.Device.android_phone, 19);
+//
+//        String movieName = movies.get(0).get("franchiseName");
+//        new DishAnywhereHome()
+//                .openSettings()
+//                .openParentalControls("1111")
+//                .setMovieGBlocked()
+//                .setTVYBlocked()
+//                .save()
+//                .openOnDemand()
+//                .searchFor(movieName)
+//
+//                .openProtectedMovie(movieName)
+//                .enterPasscode("1111")
+//                .watchMovie()
+//                .verifyMoviePlays();
+//
+//    }
+//}
