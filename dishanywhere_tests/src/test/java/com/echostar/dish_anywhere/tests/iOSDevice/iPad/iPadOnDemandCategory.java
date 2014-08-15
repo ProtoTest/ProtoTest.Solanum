fa2d56af61590ba9c20cd568af14c89e3b15dcae
Andrew Taggart <ataggart@prototest.com>
2014-08-15 09:46:06 -0600

    commnet out ipad tests for now

--- dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/iOSDevice/iPad/iPadOnDemandCategory.java
+++ dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/iOSDevice/iPad/iPadOnDemandCategory.java
@@ -1,121 +1,121 @@
-package com.echostar.dish_anywhere.tests.iOSDevice.iPad;
-
-import com.echostar.dish_anywhere.radish.RadishScraper;
-import com.echostar.dish_anywhere.screenobjects.iOSDevice.iPad.DishAnywhereHome;
-import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
-/**
- * Created by Brian on 6/4/2014.
- */
-public class iPadOnDemandCategory extends iPadTestBase {
-
-    private final int MOVIES_TO_TEST = 6;
-
-    @Test
-    public void movieCategory(){
-
-        RadishScraper radishScraper = new RadishScraper();
-        List<Map<String, String>> movies = radishScraper.getMoviesCategory(RadishScraper.Device.android_tablet, 30);
-
-        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
-
-        for (int i = 0; i < MOVIES_TO_TEST; i++) {
-            movieTitles.add(movies.get(i).get("franchiseName"));
-        }
-        new DishAnywhereHome()
-                .openOnDemand()
-                .openMovies()
-                .openFilter()
-                .sortByTitle()
-                .done()
-                .verifyTitlesPresent(movieTitles);
-
-    }
-
-    @Test
-    public void featuredCategory(){
-
-
-        RadishScraper radishScraper = new RadishScraper();
-        List<Map<String, String>> movies = radishScraper.getOnDemandFeatured(RadishScraper.Device.android_phone, 19);
-
-        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
-
-        for (int i = 0; i < MOVIES_TO_TEST; i++) {
-            movieTitles.add(movies.get(i).get("franchiseName"));
-        }
-
-        new DishAnywhereHome()
-                .openOnDemand()
-                .openFeatured()
-                .openFilter()
-                .sortByTitle()
-                .done()
-                .verifyTitlesPresent(movieTitles);
-
-    }
-
-    @Test
-    public void familyCategory(){
-
-
-        RadishScraper radishScraper = new RadishScraper();
-        List<Map<String, String>> movies = radishScraper.getFamilyCategory(RadishScraper.Device.android_tablet, 30);
-
-        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
-
-        for (int i = 0; i < MOVIES_TO_TEST; i++) {
-            movieTitles.add(movies.get(i).get("franchiseName"));
-        }
-
-        new DishAnywhereHome()
-                .openOnDemand()
-                .openFamily()
-                .openFilter()
-                .sortByTitle()
-                .done()
-                .verifyTitlesPresent(movieTitles);
-
-    }
-
-    @Test
-    public void tvShowsCategory(){
-
-
-        RadishScraper radishScraper = new RadishScraper();
-        List<Map<String, String>> movies = radishScraper.getShowsCategory(RadishScraper.Device.android_tablet, 30);
-
-        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
-
-        for (int i = 0; i < MOVIES_TO_TEST; i++) {
-            movieTitles.add(movies.get(i).get("franchiseName"));
-        }
-
-        new DishAnywhereHome()
-                .openOnDemand()
-                .openTVShows()
-                .openFilter()
-                .sortByTitle()
-                .done()
-                .verifyTitlesPresent(movieTitles);
-
-    }
-
-    @Test
-    public void networksCategory(){
-
-        List<String> movieTitles = Arrays.asList("BabyFirstTV", "Cinemax", "Content Media", "Cookie Jar", "Echo Bridge", "Encore");
-
-
-        new DishAnywhereHome()
-                .openOnDemand()
-                .openNetworks()
-                .verifyTitlesPresent(movieTitles);
-
-    }
-}
+//package com.echostar.dish_anywhere.tests.iOSDevice.iPad;
+//
+//import com.echostar.dish_anywhere.radish.RadishScraper;
+//import com.echostar.dish_anywhere.screenobjects.iOSDevice.iPad.DishAnywhereHome;
+//import org.testng.annotations.Test;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * Created by Brian on 6/4/2014.
+// */
+//public class iPadOnDemandCategory extends iPadTestBase {
+//
+//    private final int MOVIES_TO_TEST = 6;
+//
+//    @Test
+//    public void movieCategory(){
+//
+//        RadishScraper radishScraper = new RadishScraper();
+//        List<Map<String, String>> movies = radishScraper.getMoviesCategory(RadishScraper.Device.android_tablet, 30);
+//
+//        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
+//
+//        for (int i = 0; i < MOVIES_TO_TEST; i++) {
+//            movieTitles.add(movies.get(i).get("franchiseName"));
+//        }
+//        new DishAnywhereHome()
+//                .openOnDemand()
+//                .openMovies()
+//                .openFilter()
+//                .sortByTitle()
+//                .done()
+//                .verifyTitlesPresent(movieTitles);
+//
+//    }
+//
+//    @Test
+//    public void featuredCategory(){
+//
+//
+//        RadishScraper radishScraper = new RadishScraper();
+//        List<Map<String, String>> movies = radishScraper.getOnDemandFeatured(RadishScraper.Device.android_phone, 19);
+//
+//        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
+//
+//        for (int i = 0; i < MOVIES_TO_TEST; i++) {
+//            movieTitles.add(movies.get(i).get("franchiseName"));
+//        }
+//
+//        new DishAnywhereHome()
+//                .openOnDemand()
+//                .openFeatured()
+//                .openFilter()
+//                .sortByTitle()
+//                .done()
+//                .verifyTitlesPresent(movieTitles);
+//
+//    }
+//
+//    @Test
+//    public void familyCategory(){
+//
+//
+//        RadishScraper radishScraper = new RadishScraper();
+//        List<Map<String, String>> movies = radishScraper.getFamilyCategory(RadishScraper.Device.android_tablet, 30);
+//
+//        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
+//
+//        for (int i = 0; i < MOVIES_TO_TEST; i++) {
+//            movieTitles.add(movies.get(i).get("franchiseName"));
+//        }
+//
+//        new DishAnywhereHome()
+//                .openOnDemand()
+//                .openFamily()
+//                .openFilter()
+//                .sortByTitle()
+//                .done()
+//                .verifyTitlesPresent(movieTitles);
+//
+//    }
+//
+//    @Test
+//    public void tvShowsCategory(){
+//
+//
+//        RadishScraper radishScraper = new RadishScraper();
+//        List<Map<String, String>> movies = radishScraper.getShowsCategory(RadishScraper.Device.android_tablet, 30);
+//
+//        List<String> movieTitles = new ArrayList<String>(MOVIES_TO_TEST);
+//
+//        for (int i = 0; i < MOVIES_TO_TEST; i++) {
+//            movieTitles.add(movies.get(i).get("franchiseName"));
+//        }
+//
+//        new DishAnywhereHome()
+//                .openOnDemand()
+//                .openTVShows()
+//                .openFilter()
+//                .sortByTitle()
+//                .done()
+//                .verifyTitlesPresent(movieTitles);
+//
+//    }
+//
+//    @Test
+//    public void networksCategory(){
+//
+//        List<String> movieTitles = Arrays.asList("BabyFirstTV", "Cinemax", "Content Media", "Cookie Jar", "Echo Bridge", "Encore");
+//
+//
+//        new DishAnywhereHome()
+//                .openOnDemand()
+//                .openNetworks()
+//                .verifyTitlesPresent(movieTitles);
+//
+//    }
+//}
--- dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/iOSDevice/iPad/iPadVideoDrm.java
+++ dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/iOSDevice/iPad/iPadVideoDrm.java
@@ -1,50 +1,50 @@
-package com.echostar.dish_anywhere.tests.iOSDevice.iPad;
-
-import com.echostar.dish_anywhere.radish.RadishScraper;
-import com.echostar.dish_anywhere.screenobjects.iOSDevice.iPad.DishAnywhereHome;
-import com.prototest.solanum.Logger;
-import com.prototest.solanum.Verifications;
-import org.testng.annotations.Test;
-
-// DishAnywhere API Tests - GalaxyS5 (Android Phone)
-
-public class iPadVideoDrm extends iPadTestBase {
-
-
-    @Test
-    public void watchNagraMovie() {
-        RadishScraper radishScraper = new RadishScraper();
-        radishScraper.getMovies();
-        String movie = radishScraper.findMovieWithDrm("nagra");
-        watchMovie(movie);
-
-        Verifications.addVerification("Watched onDemand nagra movie.", true);
-        Verifications.assertVerifications();
-
-    }
-
-    @Test
-    public void watchWidevineMovie() {
-        RadishScraper radishScraper = new RadishScraper();
-        radishScraper.getMovies();
-        String movie = radishScraper.findMovieWithDrm("widevine");
-        watchMovie(movie);
-
-        Verifications.addVerification("Watched onDemand widevine movie.", true);
-        Verifications.assertVerifications();
-
-    }
-
-    private void watchMovie(String movie) {
-        new DishAnywhereHome()
-                .openOnDemand()
-                .searchFor(movie)
-                .openOnDemandResults()
-                .openMovie(movie)
-                .watchMovie()
-                .openControls()
-                .verifyMoviePlays();
-        Logger.screenshot("OnDemandMovie");
-
-    }
-}
+//package com.echostar.dish_anywhere.tests.iOSDevice.iPad;
+//
+//import com.echostar.dish_anywhere.radish.RadishScraper;
+//import com.echostar.dish_anywhere.screenobjects.iOSDevice.iPad.DishAnywhereHome;
+//import com.prototest.solanum.Logger;
+//import com.prototest.solanum.Verifications;
+//import org.testng.annotations.Test;
+//
+//// DishAnywhere API Tests - GalaxyS5 (Android Phone)
+//
+//public class iPadVideoDrm extends iPadTestBase {
+//
+//
+//    @Test
+//    public void watchNagraMovie() {
+//        RadishScraper radishScraper = new RadishScraper();
+//        radishScraper.getMovies();
+//        String movie = radishScraper.findMovieWithDrm("nagra");
+//        watchMovie(movie);
+//
+//        Verifications.addVerification("Watched onDemand nagra movie.", true);
+//        Verifications.assertVerifications();
+//
+//    }
+//
+//    @Test
+//    public void watchWidevineMovie() {
+//        RadishScraper radishScraper = new RadishScraper();
+//        radishScraper.getMovies();
+//        String movie = radishScraper.findMovieWithDrm("widevine");
+//        watchMovie(movie);
+//
+//        Verifications.addVerification("Watched onDemand widevine movie.", true);
+//        Verifications.assertVerifications();
+//
+//    }
+//
+//    private void watchMovie(String movie) {
+//        new DishAnywhereHome()
+//                .openOnDemand()
+//                .searchFor(movie)
+//                .openOnDemandResults()
+//                .openMovie(movie)
+//                .watchMovie()
+//                .openControls()
+//                .verifyMoviePlays();
+//        Logger.screenshot("OnDemandMovie");
+//
+//    }
+//}
