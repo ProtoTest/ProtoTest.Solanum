d1548b53d0f1cd575f6b72833c34c4d4fc24eaeb
bkitchener1 <kitchener.brian@gmail.com>
2014-08-18 10:36:17 -0600

    cleanup

--- dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/aPhone/galaxyS5/GalaxyS5LoginTests.java
+++ dishanywhere_tests/src/test/java/com/echostar/dish_anywhere/tests/aPhone/galaxyS5/GalaxyS5LoginTests.java
@@ -1,15 +1,6 @@
 package com.echostar.dish_anywhere.tests.aPhone.galaxyS5;
-
-import com.echostar.dish_anywhere.radish.RadishScraper;
-import com.echostar.dish_anywhere.screenobjects.aPhone.galaxyS5.DeviceMain;
 import com.echostar.dish_anywhere.screenobjects.aPhone.galaxyS5.DishAnywhereHome;
-import com.prototest.solanum.Config;
-import com.prototest.solanum.Logger;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.lang.annotation.Repeatable;
-import java.util.Iterator;
 
 //
 //Tests for proper functionality of basic app functionality
