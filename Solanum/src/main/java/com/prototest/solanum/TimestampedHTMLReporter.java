68569b4540bb49a735dffb8ffd19559f8c05df0b
Andrew Taggart <ataggart@prototest.com>
2014-08-25 19:20:31 -0600

    Remove nio reference

--- Solanum/src/main/java/com/prototest/solanum/TimestampedHTMLReporter.java
+++ Solanum/src/main/java/com/prototest/solanum/TimestampedHTMLReporter.java
@@ -8,7 +8,6 @@ import org.uncommons.reportng.HTMLReporter;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
