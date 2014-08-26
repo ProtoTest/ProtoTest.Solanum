f3fee1a7e580315fc31d4a4dcb7e44e80500c2f5
Andrew Taggart <ataggart@prototest.com>
2014-08-26 09:17:38 -0600

    Reorder suite setup initialization order

--- Solanum/src/main/java/com/prototest/solanum/EggplantTestBase.java
+++ Solanum/src/main/java/com/prototest/solanum/EggplantTestBase.java
@@ -91,12 +91,12 @@ public class EggplantTestBase {
             Config.drivePort = drivePort;
         }
 
-        driver = new EggplantDriver();
         Logger.info("Creating report directory.");
         createReportDirectory();
         startEggplant();
         setEggplantDefaultSettings();
         Logger.info(String.format("Connecting to eggdrive: %s:%s", Config.driveUrl, Config.drivePort));
+        driver = new EggplantDriver();
         driver.connect(hostName, hostPort);
     }
 
