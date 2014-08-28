6a13735c64a7d88b34441e16f60fcaa6d633af26
Andrew Taggart <ataggart@prototest.com>
2014-08-28 08:30:26 -0600

    Improve title detection

--- DishAnywhere_S5/src/test/java/com/echostar/dish_anywhere/screenobjects/aPhone/galaxyS5/DishAnywhereScrollView.java
+++ DishAnywhere_S5/src/test/java/com/echostar/dish_anywhere/screenobjects/aPhone/galaxyS5/DishAnywhereScrollView.java
@@ -1,10 +1,8 @@
 package com.echostar.dish_anywhere.screenobjects.aPhone.galaxyS5;
 
-import com.prototest.solanum.By;
-import com.prototest.solanum.EggplantElement;
-import com.prototest.solanum.Logger;
-import com.prototest.solanum.TextOption;
+import com.prototest.solanum.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishAnywhereScrollView extends DishAnywhereMain {
@@ -37,21 +35,49 @@ public class DishAnywhereScrollView extends DishAnywhereMain {
     }
 
     public DishAnywhereScrollView verifyTitlesPresent(List<String> titles){
+        String logMessage = "Verifying movies from radish are present: " + titles.get(0);
+        for (String title : titles.subList(1, titles.size() - 1)) {
+            logMessage += ", " + title;
+        }
+        Logger.info(logMessage);
         popups.waitForScreenToLoad();
         List<EggplantElement> movieArrows = movieArrow.allInstances();
+
+        List<String> notFound = new ArrayList<String>(titles);
         for (int i = 0; i < titles.size(); i++) {
+            Logger.info("Looking for movie title in position "+ (i+1) +": (" + titles.get(i) + ")");
+            boolean passes = false;
             movieArrows.get(i).click();
-            EggplantElement movie = new EggplantElement(By.Text(titles.get(i)));
+            String searchingFor = titles.get(i);
+            EggplantElement movieTitle = new EggplantElement(By.Text(searchingFor));
             EnterPasscodePopup passcode = new EnterPasscodePopup();
             if(passcode.isPasscodePresent()){
                 Logger.error("Passcode present for movie : " + titles.get(i).toString() + " when it should not be");
-                passcode.enterPasscode("1111");
+                passcode.enterPasscode(Config.getTestProp("dishAnywherePassCode"));
             }
-            if(!movie.isPresent()){
-                 movie = new EggplantElement(By.Text(titles.get(i),TextOption.language("Spanish")));
-                 movie.verifyPresent();
+            if (movieTitle.isPresent()) {
+                Logger.info("Found movie title " + searchingFor + " on screen");
+                passes = true;
+                notFound.remove(searchingFor);
+            } else {
+                Logger.error(String.format("Expected movie %s to appear in position %d", searchingFor, i));
+                Verifications.addVerification(String.format("Movie %s should appear in position %d", searchingFor, i), false, false);
+                Logger.info("Trying to match any other movie in the movies list");
+                for (String title : notFound) {
+                    if (new EggplantElement(By.Text(title, TextOption.waitFor(5))).isPresent()) {
+                        Logger.info("Matched out of order movie: " + title);
+                        passes = true;
+                        notFound.remove(title);
+                        break;
+                    }
+                }
+
             }
+            Verifications.addVerification(String.format("Movie %s should be present.", searchingFor), passes, false);
             nav.backButton.click();
+        }
+        for (String notFoundTitle : notFound) {
+            Verifications.addVerification(String.format("Movie %s should be present.", notFoundTitle), false, false);
         }
         return this;
     }
